da78a31fbe4917779a89c6e38ad69d882ad8eec4
Konstantinos Zoumpatianos <zoumpatianos@ariadne.science.unitn.it>
2013-11-20 09:59:03 +0100

    Run experiments changes

--- run_experiments.py
+++ run_experiments.py
@@ -6,12 +6,14 @@ if __name__ == "__main__":
     import sys
     parser = BinaryParser(sys.argv[1], int(sys.argv[2]))
     experiment = InMemoryDistancesExperiment(normalize=True, window=None)
-    experiment_output_container = FileContainer("output.txt", binary=False)
 
     print "Loading data..."
     experiment.load_data(parser, 100)
     print "Loaded: %d time series." % len(experiment.dataset)
+
+
     print "Running queries..."
+    experiment_output_container = FileContainer("output.txt", binary=False)
     for query_result in experiment.run_queries(10, noise=0.1):
         experiment_output_container.write(query_result)
     experiment_output_container.close()
