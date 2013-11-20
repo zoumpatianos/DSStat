700134fc84563afd8eeea68a1ac5a1074ead5255
Konstantinos Zoumpatianos <zoumpatianos@ariadne.science.unitn.it>
2013-11-20 13:19:46 +0100

    Added experiment resuls

---
+++ experiments/results/results_directory.py
@@ -0,0 +1,18 @@
+import os
+from experiments.results.utils.make_html_page import makeHTMLpage
+
+class ResultsDirectory(object):
+    def __init__(self, name):
+        self.name = name
+        os.mkdir(name)
+    def create_filename(self, filename):
+        return os.path.join(self.name, filename)
+    def create_listing(self):
+        index = makeHTMLpage(self.name)
+        index_file = open(self.create_filename("index.html"), "w")
+        index_file.write(index)
+        index_file.close()
+    def zip_directory(self, target_file):
+        pass
+    def copy_directory(self, target):
+        pass
