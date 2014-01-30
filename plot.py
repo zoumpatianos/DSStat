dba53ebda7155b61fb028b2ef6bf8cbd31d3a3c8
Konstantinos Zoumpatianos <zoumpatianos@ariadne.science.unitn.it>
2014-01-30 23:55:50 +0100

    All

--- plot.py
+++ plot.py
@@ -1,8 +1,30 @@
 from tsbench.plot.timeseries_plot import TimeSeriesPlot
+from tsbench.parsers.binary_parser import BinaryParser
+from tsbench.net.scp import SCP
+import sys
+import numpy as np
 
 if __name__ == "__main__":
-    plot = TimeSeriesPlot()
-    plot.add_timeseries([1,2,5,6,3,2,9,8,1,5,2,3,6,8])
-    plot.add_timeseries([3,6,8,7,8,9,3,1,4,5,6,8,7,1])
-    plot.save("plot.png")
+    dataset = sys.argv[1]
+    datalength = int(sys.argv[2])
+    plotfile = sys.argv[3]
+    if len(sys.argv) > 4:
+        normalized = (sys.argv[4] == "normalize")
+    else:
+        normalized = False
 
+    plot = TimeSeriesPlot()
+    parser = BinaryParser(dataset, datalength)
+    count = 0
+    for ts in parser.parse():
+        count += 1
+        if count % 1000 == 0:
+            print "Processed: %d" % (count)
+        if normalized:
+            normts = (ts - np.mean(ts)) / np.std(ts)
+            plot.add_timeseries(normts)
+        else:
+            plot.add_timeseries(ts)
+    plot.save(plotfile)
+    scp = SCP("disi.unitn.it")
+    scp.copy(plotfile, "public_html/")
